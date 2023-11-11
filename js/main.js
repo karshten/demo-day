bb976b37f2c399832627e47c31971abcf3e9fb4a
oskar <88786315+karshten@users.noreply.github.com>
2023-11-11 12:45:15 +0600

    updated link for BE to auth

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ const bindPostData = (form) => {
             return alert('Вы не повторили пароль!')
         }
         if (password.value === passwordRepeat.value) {
-            return postData('https://9959-176-123-255-178.ngrok-free.app/api/v1/users/register/', json)
+            return postData('https://33ee-212-112-111-56.ngrok-free.app/auth/register/', json)
         }
         else {
             return alert('Пароли не совпали')
